3fae82923513ee8d312346b5ed7969a7344c04ed
Anshikabharadwaj <Anshikabharadwaj32@gmail.com>
2025-05-22 13:59:23 +0530

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,75 @@
+package String;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+//Q1
+//		String original ="Java Exercise";
+//		char ch = original.charAt(0);
+//		char chh = original.charAt(5);
+//		char chhh = original.charAt(13);
+//
+//		System.out.println(ch);
+//		System.out.println(chh);
+//		System.out.println(chhh);
+//		
+	
+//Q2
+//		String a = "PHP, Exercises";
+//		String b = " Python Exercise";
+//		System.out.println(a.concat(b));
+//	
+		
+//Q3
+//	String Original = " PHP exercises and python exercises Specified sequence of char values : and true";
+//	String x = " Specified sequence of char values";
+//	
+//	System.out.println(Original.contains(x));
+		
+//Q4
+////		Date dt = new Date();
+//		LocalDateTime now = LocalDateTime.now();
+//		
+//		DateTimeFormatter f = DateTimeFormatter.ofPattern("MMMM dd , yyyy h:mm a", Locale.ENGLISH);
+//		
+//		String Datetime = now.format(f).replace("AM", "am").replace("PM", "pm");
+//		System.out.println(Datetime);
+//		
+//Q5
+//		String ar1 = "A b c d e f g h I j";
+//		char ar[] = {'A','a','b','c','d','e','f','g','h','I','j'};
+//		
+//		for(char ch : ar) {
+//			int index = ar1.indexOf(ch);
+//		
+//		System.out.println(ch + " Index found at : " + ar1);
+//		}
+		
+//Q6
+//		String ss = "example.com";
+//		
+//			System.out.println(ss.length());
+
+//Q9
+//		String ss = "we3resource";
+//		String s1= "";
+//		
+//		for(int i=0; i<=ss.length();i++) {
+//		
+//		}
+//		System.out.println("Original string : " + ss);
+//		System.out.println("After removing duplicates : " + s1);
+		
+		
+//Q10.
+//		
+		
+	}
+}
